0d8776d154104b3d1967189047fd29b678be0016
German Gambarte <germangambarte@gmail.com>
2025-01-29 22:28:43 -0300

    refactor: modify errors

--- exercise-01/src/App.tsx
+++ exercise-01/src/App.tsx
@@ -1,41 +1,113 @@
 import { useState } from 'react'
 import { Account } from './models/account.model'
 import * as React from 'react'
+import { TransactionForm } from './components/transaction-form'
+import { AddAccountForm } from './components/add-account-form'
 
-type Actions = 'EXTRACT' | 'DEPOSIT'
+export type Actions =
+  | 'EXTRACT'
+  | 'DEPOSIT'
+  | 'ADD-ACCOUNT'
+  | 'SHOW-ADD-ACCOUNT-FORM'
+  | 'SHOW-TRASACTION-FORM'
+
+export type ErrorTypes = 'SUCCESS' | 'ERROR'
 
 export const App: React.FC = () => {
   //TODO: Agregar cuentas de manera dinámica
-  const [accounts] = useState<Account[]>([
+  const [accounts, setAccounts] = useState<Account[]>([
     new Account(1, '20123456786', 'Micaela', 'Farias', 123),
-    new Account(2, '20410266920', 'German', 'Gambarte', 123)
+    new Account(2, '20123123124', 'German', 'Gambarte', 123)
   ])
   const [selectedAccount, setSelectedAccount] = useState<Account | null>(null)
   const [selectedAction, setSelectedAction] = useState<Actions | null>(null)
   const [amountOperation, setAmountOperation] = useState<number | null>(null)
+  const [newAccountData, setNewAccountData] = useState({
+    cuil: '',
+    firstName: '',
+    lastName: '',
+    balance: 0
+  })
+  const [info, setInfo] = useState<{
+    type: string
+    message: string
+  } | null>(null)
 
   function handleClick(id: number) {
     const findAccount = accounts.find((account) => account.id == id)
+    if (!findAccount) {
+      setInfo({ type: 'ERROR', message: 'Cuenta no encontrada.' })
+      return
+    }
     setSelectedAccount(findAccount)
+    setAmountOperation(null)
+    setSelectedAction('SHOW-TRASACTION-FORM')
   }
 
   function handleChangeAmount(e: React.ChangeEvent<HTMLInputElement>) {
-    const value = Number.isNaN(e.target.value) ? null : e.target.valueAsNumber
-    setAmountOperation(value)
+    const value = e.target.valueAsNumber
+    setAmountOperation(Number.isNaN(value) ? null : value)
+  }
+
+  function handleChangeAddNewAccountState(
+    e: React.ChangeEvent<HTMLInputElement>
+  ) {
+    const value = e.target.value
+    setNewAccountData({
+      ...newAccountData,
+      [e.target.name]: e.target.type === 'text' ? value : Number(value)
+    })
+  }
+
+  function handleAddNewAccount(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const { data, info } = Account.verifyCuil(newAccountData.cuil)
+    if (
+      newAccountData.firstName.length < 2 ||
+      newAccountData.lastName.length < 1
+    ) {
+      setInfo({
+        type: 'ERROR',
+        message: 'El nombre y apellido debe tener mínimo 2 caracteres'
+      })
+      return
+    }
+    if (!data) {
+      setInfo(info)
+      return
+    }
+    const newAccount = new Account(
+      accounts.length + 1,
+      newAccountData.cuil,
+      newAccountData.firstName,
+      newAccountData.lastName,
+      newAccountData.balance
+    )
+    setInfo(null)
+    setSelectedAction(null)
+    setAccounts([...accounts, newAccount])
   }
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    //TODO: Actualizar los valores al momento de realizar la acción
-    //TODO: Mostrar Errores en caso de haberlos
     e.preventDefault()
-    switch (selectedAction) {
-      case 'DEPOSIT':
-        selectedAccount.tryDeposit(amountOperation)
-        break
-      case 'EXTRACT':
-        selectedAccount.tryExtract(amountOperation)
-        break
+    if (!selectedAction || !amountOperation || amountOperation <= 0) {
+      setInfo({ type: 'ERROR', message: 'Ingrese un monto válido.' })
+      return
     }
+    if (selectedAction == 'DEPOSIT') {
+      const { info } = selectedAccount.tryDeposit(amountOperation)
+      if (info?.type === 'ERROR') {
+        setInfo(info)
+      }
+    }
+    if (selectedAction == 'EXTRACT') {
+      const { info } = selectedAccount.tryExtract(amountOperation)
+      if (info?.type === 'ERROR') {
+        setInfo(info)
+      }
+    }
+    setInfo(info)
+    setAmountOperation(null)
   }
   return (
     <>
@@ -43,55 +115,52 @@ export const App: React.FC = () => {
         Administrador de cuentas
       </h1>
       <main className="max-w-[1200px] mx-auto flex justify-around">
-        <div className="flex-1 flex flex-col gap-4">
+        <div className="flex-1 flex flex-col gap-4 mt-8">
           {accounts.map((account) => (
             <button
               key={account.id}
-              className="bg-slate-100 p-4 shadow-md cursor-pointer hover:bg-slate-200/60 rounded-xl"
+              className="bg-slate-100 p-4 shadow-md cursor-pointer hover:bg-slate-200 rounded-xl"
               onClick={() => handleClick(account.id)}
             >
               {account.firstName} {account.lastName}
             </button>
           ))}
+          <button
+            className="bg-blue-700 px-4 py-2 shadow-md cursor-pointer text-white font-bold hover:bg-blue-800 rounded-xl"
+            onClick={() => setSelectedAction('SHOW-ADD-ACCOUNT-FORM')}
+          >
+            +
+          </button>
         </div>
         <div className="flex-1">
-          {selectedAccount ? (
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col justify-center items-center gap-4 mb-8"
-            >
-              <input
-                type="number"
-                name="amount"
-                placeholder="0"
-                onChange={handleChangeAmount}
-                value={amountOperation ?? ''}
-                className="w-md text-center mt-4 text-8xl outline-none border-b-2
-                    [appearance:textfield]
-                    [&::-webkit-outer-spin-button]:appearance-none
-                    [&::-webkit-inner-spin-button]:appearance-none"
-              />
-              <p className="text-black/60">
-                Saldo disponible: $ {selectedAccount.balance.toFixed(2)}
-              </p>
-              <div className="flex justify-center gap-4">
-                <button
-                  onClick={() => setSelectedAction('EXTRACT')}
-                  className="bg-blue-500 hover:bg-blue-500/60 p-4 text-white font-bold rounded-xl cursor-pointer"
-                >
-                  Extraer Dinero
-                </button>
-                <button
-                  onClick={() => setSelectedAction('DEPOSIT')}
-                  className="bg-blue-500 hover:bg-blue-500/60 p-4 text-white font-bold rounded-xl cursor-pointer"
-                >
-                  Depositar Dinero
-                </button>
-              </div>
-            </form>
-          ) : (
-            <p className="text-center">
+          {selectedAction === 'SHOW-ADD-ACCOUNT-FORM' && (
+            <AddAccountForm
+              handleAddNewAccount={handleAddNewAccount}
+              handleChangeAddNewAccountState={handleChangeAddNewAccountState}
+            />
+          )}
+          {selectedAction !== 'SHOW-ADD-ACCOUNT-FORM' && selectedAccount && (
+            <TransactionForm
+              handleSubmit={handleSubmit}
+              handleChangeAmount={handleChangeAmount}
+              selectedAccount={selectedAccount}
+              amountOperation={amountOperation}
+              setSelectedAction={setSelectedAction}
+            />
+          )}
+          {!selectedAction && (
+            <p className="max-w-lg mx-auto mt-4 bg-slate-500/20 text-slate-800 border-1 border-slate-800 rounded-xl p-4 text-center">
               Para realizar una operación, primero selecciones una cuenta.
+            </p>
+          )}
+          {info?.type === 'ERROR' && (
+            <p className="max-w-lg mx-auto text-center bg-red-500/20 text-red-500 border-1 border-red-500 rounded-xl p-4">
+              {info.message}
+            </p>
+          )}
+          {info?.type === 'SUCCESS' && (
+            <p className="max-w-lg mx-auto text-center bg-green-500/20 text-green-500 border-1 border-green-500 rounded-xl p-4">
+              La transación se realizó con éxito.
             </p>
           )}
         </div>
--- exercise-01/src/models/account.model.ts
+++ exercise-01/src/models/account.model.ts
@@ -1,3 +1,5 @@
+const VALID_GENRES = [20, 23, 27, 30]
+const VALIDATION_FACTORS = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
 export class Account {
   private _id: number
   private _cuil: string
@@ -23,13 +25,19 @@ export class Account {
     if (amount < 0) {
       return {
         data: null,
-        errors: 'El importe ingresado debe ser mayor a $0,00'
+        info: {
+          type: 'ERROR',
+          message: 'El importe ingresado debe ser mayor a $0,00'
+        }
       }
     }
     this._balance += amount
     return {
       data: this._balance,
-      errors: null
+      info: {
+        type: 'SUCCESS',
+        message: 'La trasacción se realizó con éxito'
+      }
     }
   }
 
@@ -37,20 +45,87 @@ export class Account {
     if (amount < 0) {
       return {
         data: null,
-        errors: 'El importe ingresado debe ser mayor a $0,00'
+        info: {
+          type: 'ERROR',
+          message: 'El importe ingresado debe ser mayor a $0,00'
+        }
       }
     }
     if (amount > this._balance) {
       return {
         data: null,
-        errors: 'Saldo insuficiente para realizar la transacción'
+        info: {
+          type: 'ERROR',
+          message: 'Saldo insuficiente para realizar la transacción'
+        }
       }
     }
 
     this._balance -= amount
     return {
       data: this._balance,
-      errors: null
+      info: {
+        type: 'SUCCESS',
+        message: 'La trasacción se realizó con éxito'
+      }
+    }
+  }
+
+  static verifyCuil(cuil: string) {
+    if (cuil.length != 11) {
+      return {
+        data: false,
+        info: {
+          type: 'ERROR',
+          message: 'El número de cuil debe contener 11 dígitos'
+        }
+      }
+    }
+    const validationDigit = Number(cuil.substring(10))
+    const genre = Number(cuil.substring(0, 2))
+    if (!VALID_GENRES.includes(genre)) {
+      return {
+        data: false,
+        info: {
+          type: 'ERROR',
+          message: 'Cuil no válido. (no valid genre)'
+        }
+      }
+    }
+
+    let result = 0
+    for (let i = 0; i < VALIDATION_FACTORS.length; i++) {
+      result += Number(cuil[i]) * VALIDATION_FACTORS[i]
+    }
+    const restOfResult = result % 11
+
+    if (restOfResult === 0 && validationDigit === 0) {
+      return {
+        data: true,
+        info: null
+      }
+    }
+
+    if (
+      restOfResult === 1 &&
+      genre === 23 &&
+      (validationDigit === 9 || validationDigit === 4)
+    ) {
+      return {
+        data: true,
+        info: null
+      }
+    }
+    if (validationDigit === 11 - restOfResult) {
+      return {
+        data: true,
+        info: null
+      }
+    } else {
+      return {
+        data: false,
+        info: { type: 'ERROR', message: 'Cuil no válido' }
+      }
     }
   }
 
@@ -63,18 +138,23 @@ export class Account {
       balance: this._balance
     }
   }
+
   get id() {
     return this._id
   }
+
   get cuil() {
     return this._cuil
   }
+
   get firstName() {
     return this._firstName
   }
+
   get lastName() {
     return this._lastName
   }
+
   get balance() {
     return this._balance
   }
